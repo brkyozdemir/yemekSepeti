c1ebb87d1854aa9214278e5a734a86ff33c990a5
brkyozdemir <berkayozdemir02@gmail.com>
2022-02-13 19:49:11 +0300

    read json file and assign it to out store.

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"github.com/yemekSepeti/internal/database"
 	"github.com/yemekSepeti/internal/repository/concrete"
 	"github.com/yemekSepeti/internal/routes"
+	"github.com/yemekSepeti/internal/utils"
 )
 
 func main() {
 	db := concrete.NewMemDB()
+
+	store := utils.ReadFromResources()
+	db.Store = store
 
 	api := database.API{
 		Db: db,
