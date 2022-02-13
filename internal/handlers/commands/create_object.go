4970f285acf4686ecf93b655ccc15ae39d7f09ff
brkyozdemir <berkayozdemir02@gmail.com>
2022-02-13 18:51:03 +0300

    implement create_object handler(controller)

---
+++ internal/handlers/commands/create_object.go
@@ -0,0 +1,26 @@
+package commands
+
+import (
+	"encoding/json"
+	"github.com/yemekSepeti/internal/database"
+	"log"
+	"net/http"
+)
+
+func CreateObject(api database.API) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var body map[string]string
+		err := json.NewDecoder(r.Body).Decode(&body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var object map[string]string
+		for key, value := range body {
+			object = api.Db.CreateObject(key, value)
+		}
+
+		json.NewEncoder(w).Encode(object)
+
+	}
+}
