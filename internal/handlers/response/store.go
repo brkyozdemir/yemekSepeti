fb41939fa8bb5c87fe98518a7acdefd68bb8b2a3
brkyozdemir <berkayozdemir02@gmail.com>
2022-02-13 21:19:44 +0300

    update response package.

--- internal/handlers/response/store.go
+++ internal/handlers/response/store.go
@@ -1,12 +1,12 @@
 package response
 
-type Object struct {
-	Key   string `json:"key"`
-	Value string `json:"value"`
-}
-
 // To transform the response (hide some paramters or add from anywhere else etc.), we may need this function.
 // Right now i am just using it to convert model to json.
+
+//type Object struct {
+//	Key   string `json:"key"`
+//	Value string `json:"value"`
+//}
 
 //func (o *Object) MapToResponse(model Model, ...) {
 //	o.Key = model.key
