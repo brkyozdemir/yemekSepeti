3ca685a4b08107d3594b469d23d254920b81cc54
brkyozdemir <berkayozdemir02@gmail.com>
2022-02-13 22:35:19 +0300

    add refresh db

--- internal/utils/test.go
+++ internal/utils/test.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"github.com/yemekSepeti/internal/database"
 	"github.com/yemekSepeti/internal/repository/concrete"
+	"net/http"
 )
 
 func GetTestDB() database.API {
@@ -12,4 +13,9 @@ func GetTestDB() database.API {
 	}
 
 	return api
+}
+
+func Refresh(client *http.Client) {
+	refresh, _ := http.NewRequest(http.MethodDelete, "http://localhost:9000/api/keys", nil)
+	_, _ = client.Do(refresh)
 }
