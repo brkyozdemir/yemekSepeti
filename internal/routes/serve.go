2618a8aa6920421f4b38745e210d9f489c1ce027
brkyozdemir <berkayozdemir02@gmail.com>
2022-02-13 18:53:33 +0300

    serve routes that is created dynamically.

---
+++ internal/routes/serve.go
@@ -0,0 +1,40 @@
+package routes
+
+import (
+	"context"
+	"fmt"
+	"github.com/yemekSepeti/internal/utils"
+	"net/http"
+	"strings"
+)
+
+func Serve(routes []route) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var allow []string
+
+		w.Header().Set("Content-Type", "application/json")
+		for _, route := range routes {
+			matches := route.regex.FindStringSubmatch(r.URL.Path)
+			if len(matches) > 0 {
+				if r.Method != route.method {
+					allow = append(allow, route.method)
+					continue
+				}
+				ctx := context.WithValue(r.Context(), utils.ContexKey{}, matches[1:])
+				route.handler(w, r.WithContext(ctx))
+				return
+			}
+		}
+		if len(allow) > 0 {
+			w.Header().Set("Allow", strings.Join(allow, ", "))
+			http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		http.NotFound(w, r)
+	}
+}
+
+func Start(routes []route) {
+	fmt.Println("Route is starting...")
+	http.ListenAndServe(":9000", Serve(routes))
+}
